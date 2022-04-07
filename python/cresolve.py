fe5f61a30a293123ad58b6193aab8c47bad7abb0
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2022-04-07 19:07:20 +0100

    Add files via upload

---
+++ python/cresolve.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/python
+
+import time
+import os
+import sys
+import requests
+
+print("CloudFare Resolver")
+
+target=input('\bEnter Target : ')
+r=requests.get('https://dns.google.com/query?name=' + target)
+print('\bResult:')
+print(r.txt)
