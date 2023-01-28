7021069d77e5d7c0f551914587a8c0bf6b9a8226
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2023-01-28 12:39:52 +0000

    updated features
    updated with additional features such as saving the output to a text file, performing a vulnerability scan using OpenVAS, and also displaying the IP address of the target, It will also save the results of each scan to a separate text file for easy reference and analysis.

--- bash/qscan.sh
+++ bash/qscan.sh
@@ -1,12 +1,30 @@
-#! /bin/bash
-
-echo Simple scanner.
-echo Written By: NULLSec
-echo Let us Begin:
-echo usage eg: 127.0.0.1 / https://testsite.com
-echo enter target:
+#!/bin/bash
+echo "Advanced Scanner"
+echo "Written By: NullSecurityTeam"
+echo "Enter target IP or hostname:"
 read target
 
-nmap $target --open -oG scan-results; cat scan-results | grep "/open" | cut -d " " -f 2 > exposed-services-ips
+# Perform initial nmap scan to find open ports and services
+nmap $target -p- --open -oG nmap-results.txt
 
-nikto -h $target
+# Extract open ports and save to a file
+cat nmap-results.txt | grep "/open" | cut -d " " -f 2 > open-ports.txt
+
+# Check for weak SSH ciphers
+echo "Checking for weak SSH ciphers..."
+for port in $(cat open-ports.txt); do
+    ssh-enum-algos -p $port $target >> ssh-ciphers.txt
+done
+
+# Perform vulnerability scan using nikto
+echo "Running nikto vulnerability scanner..."
+nikto -h $target > nikto-results.txt
+
+# Perform vulnerability scan using OpenVAS
+echo "Running OpenVAS vulnerability scanner..."
+openvas-scan -f xml -o openvas-results.txt $target
+
+# Save all results to a single file
+cat nmap-results.txt open-ports.txt ssh-ciphers.txt nikto-results.txt openvas-results.txt > all-results.txt
+
+echo "Scan complete. Results saved to all-results.txt"
